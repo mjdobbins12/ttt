ee24e957026d0dda42ae0900bbaae34bedfdd34c
Mark Dobbins <mjdobbins12@gmail.com>
2019-11-12 20:42:06 +0000

    computer player blocks human from completing a column

--- lib/single_player.rb
+++ lib/single_player.rb
@@ -2,6 +2,8 @@ require 'game'
 require 'computer_player'
 
 class SinglePlayerGame < Game
+  attr_reader :victory_rows
+  
   def initialize(player1)
     @player1 = player1
     @player2 = InvincibleComputerPlayer.new(player1.sign.succ)
@@ -13,16 +15,16 @@ class SinglePlayerGame < Game
     ]
     @complete = false
     @victor = nil
-    @victory_rows = [
-      @board.slice(0..2),
-      @board.slice(3..5),
-      @board.slice(6..8),
-      [@board[0], @board[3], @board[6]],
-      [@board[1], @board[4], @board[7]],
-      [@board[2], @board[5], @board[8]],
-      [@board[0], @board[4], @board[8]],
-      [@board[2], @board[4], @board[6]]
-    ]
+    # @victory_rows = [
+    #   @board.slice(0..2),
+    #   @board.slice(3..5),
+    #   @board.slice(6..8),
+    #   [@board[0], @board[3], @board[6]],
+    #   [@board[1], @board[4], @board[7]],
+    #   [@board[2], @board[5], @board[8]],
+    #   [@board[0], @board[4], @board[8]],
+    #   [@board[2], @board[4], @board[6]]
+    # ]
   end
 
   def move(square)
@@ -45,11 +47,12 @@ class SinglePlayerGame < Game
   end
   
   def look_for_winning_moves
-    @victory_rows.each do |row|
+    victory_rows = generate_victory_rows
+    victory_rows.each do |row|
       if row.count(@player2.sign) == 2
         new_move = row.select{ |x| x != @player2.sign }
         if @board[new_move] != @player1.sign
-          @board[new_move] = @player2.sign
+          @board[new_move[0]] = @player2.sign
           break
         end
       end
@@ -57,10 +60,11 @@ class SinglePlayerGame < Game
   end
   
   def block_player1
-    @victory_rows.each do |row|
+    victory_rows = generate_victory_rows
+    victory_rows.each do |row|
       if row.count(@player1.sign) == 2
         new_move = row.select{ |x| x != @player1.sign }
-        @board[new_move] = @player2.sign
+        @board[new_move[0]] = @player2.sign
       end
     end
   end
@@ -71,5 +75,19 @@ class SinglePlayerGame < Game
     elsif @board[0] == 0
       @board[0] = @player2.sign
     end
+  end
+  
+  def generate_victory_rows
+    victory_rows = [
+      @board.slice(0..2),
+      @board.slice(3..5),
+      @board.slice(6..8),
+      [@board[0], @board[3], @board[6]],
+      [@board[1], @board[4], @board[7]],
+      [@board[2], @board[5], @board[8]],
+      [@board[0], @board[4], @board[8]],
+      [@board[2], @board[4], @board[6]]
+    ]
+    victory_rows
   end
 end
--- spec/single_player_spec.rb
+++ spec/single_player_spec.rb
@@ -15,4 +15,10 @@ describe SinglePlayerGame do
 		@spg.move(0)
 		expect(@spg.board[4]).to eq("Y")
 	end
+	
+	it 'blocks p1 from finishing a column' do
+		@spg.move(4)
+		@spg.move(1)
+		expect(@spg.board[7]).to eq("Y")
+	end
 end

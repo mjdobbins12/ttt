7d9ec1100e89c6cc5ebe6d2d17b2e2c73da14197
Mark Dobbins <mjdobbins12@gmail.com>
2019-11-12 17:44:00 +0000

    setting up for computer player moves

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -7,4 +7,16 @@ class InvincibleComputerPlayer < Player
     @name = "ICP"
     @sign = sign
   end
+  
+  def move(square)
+    unless complete?
+      @board.map! { |x| x == square ? @turn.sign : x }
+      check_victory_conditions
+      change_turns
+      icp_move
+    end
+  end
+  
+  def icp_move
+  end
 end
